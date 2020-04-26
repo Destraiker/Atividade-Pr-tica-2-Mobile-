6df66b33deae9c2593d254fb6cbfb1a063a37189
Destraiker <brener_rodrigues@hotmail.com>
2020-04-26 13:58:22 -0300

    Consertando erro bobo

--- src/app/endereco/endereco.component.ts
+++ src/app/endereco/endereco.component.ts
@@ -10,24 +10,20 @@ export class EnderecoComponent implements OnInit {
   @Input() Location;
   @Input() Nome;
   @Output() atualizaEndereco = new EventEmitter();
-  Dados = [
-    { name: this.Nome, location: { address: '', city: '', country: '' } }
-  ];
 
   constructor() { }
 
   Display() {
-    if (document.getElementById("formEndereco").style.display === "none") {
-      document.getElementById("formEndereco").style.display = "block";
-      document.getElementById("botao").innerHTML="Fechar Novo Endereço";
+    if (document.getElementById("formEndereco"+this.Nome).style.display === "none") {
+      document.getElementById("formEndereco"+this.Nome).style.display = "block";
+      document.getElementById("botao"+this.Nome).innerHTML="Fechar Novo Endereço";
     } else {
-      document.getElementById("formEndereco").style.display = "none";
-      document.getElementById("botao").innerHTML="Abrir Novo Endereço";
+      document.getElementById("formEndereco"+this.Nome).style.display = "none";
+      document.getElementById("botao"+this.Nome).innerHTML="Abrir Novo Endereço";
     }
   }
 
   Atualizar(local, cidade, pais) {
-    this.Dados=[{ name: this.Nome, location: { address: local, city: cidade, country: pais } }];
     this.atualizaEndereco.emit({name: this.Nome,location: {address: local,city:cidade, country:pais}});
   }
   ngOnInit(): void {
